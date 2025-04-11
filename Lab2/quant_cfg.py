29731abfd6db7058b730fb3e48cc4c5777f28ca8
rc1-dl3 <tony20011027@gmail.com>
2025-04-11 10:57:34 +0800

    Part2-1 Finished

--- Lab2/quant_cfg.py
+++ Lab2/quant_cfg.py
@@ -4,32 +4,41 @@ from hqq.core.quantize import BaseQuantizeConfig
 def get_quant_config_deit(model):
     quant_config = {}
     
-    # n_blocks = len(model.blocks)
-    # q2_config = BaseQuantizeConfig(nbits=2, group_size=32)
+    n_blocks = len(model.blocks)
+    q2_config = BaseQuantizeConfig(nbits=4, group_size=64)
+    q4_config = BaseQuantizeConfig(nbits=8, group_size=256)
     
-    # for i in range(n_blocks):
-    #     quant_config[f'blocks.{i}.attn.qkv'] = q2_config
-    #     quant_config[f'blocks.{i}.attn.proj'] = q2_config
-    #     quant_config[f'blocks.{i}.mlp.fc1'] = q2_config
-    #     quant_config[f'blocks.{i}.mlp.fc2'] = q2_config
-        
+    for i in range(n_blocks):
+        quant_config[f'blocks.{i}.attn.qkv'] = q2_config
+        quant_config[f'blocks.{i}.attn.proj'] = q2_config
+        quant_config[f'blocks.{i}.mlp.fc1'] = q2_config
+        quant_config[f'blocks.{i}.mlp.fc2'] = q2_config
+    
+    for i in [0, 1]:
+        quant_config[f'blocks.{i}.attn.proj'] = q4_config
+        quant_config[f'blocks.{i}.mlp.fc1'] = q4_config
+        quant_config[f'blocks.{i}.mlp.fc2'] = q4_config
+    
     return quant_config
 
 # TODO: Make your own quant config for Language Model
 def get_quant_config_slm(model):
     quant_config = {}
     
-    # n_layers = model.config.num_hidden_layers
-    # q2_config = BaseQuantizeConfig(nbits=2, group_size=64) 
-    
-    # for i in range(n_layers):
-    #     quant_config[f'model.layers.{i}.self_attn.q_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.self_attn.k_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.self_attn.v_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.self_attn.o_proj'] = q2_config
-        
-    #     quant_config[f'model.layers.{i}.mlp.gate_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.mlp.up_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.mlp.down_proj'] = q2_config
+    n_layers = model.config.num_hidden_layers
+    q_config_attn = BaseQuantizeConfig(nbits=8, group_size=64)
+    q_config_mlp = BaseQuantizeConfig(nbits=4, group_size=128)
+
+    for i in range(n_layers):
+        # Attention projections
+        quant_config[f'model.layers.{i}.self_attn.q_proj'] = q_config_attn
+        quant_config[f'model.layers.{i}.self_attn.k_proj'] = q_config_attn
+        quant_config[f'model.layers.{i}.self_attn.v_proj'] = q_config_attn
+        quant_config[f'model.layers.{i}.self_attn.o_proj'] = q_config_attn
+
+        # MLP projections
+        quant_config[f'model.layers.{i}.mlp.gate_proj'] = q_config_mlp
+        quant_config[f'model.layers.{i}.mlp.up_proj'] = q_config_mlp
+        quant_config[f'model.layers.{i}.mlp.down_proj'] = q_config_mlp
         
     return quant_config
---
+++ Lab2/run_deit_stu.py
@@ -0,0 +1,48 @@
+import torch
+import random
+import os
+import numpy as np
+
+from hqq.utils.patching import recommended_inductor_config_setter
+from hqq_utils import AutoHQQTimmModel, get_size_of_model
+from utils import prepare_data, evaluate_model
+
+from quant_cfg import get_quant_config_deit
+
+def main():
+    ############## Set Up ##############
+    torch.manual_seed(0)
+    random.seed(0)
+    np.random.seed(0)
+    recommended_inductor_config_setter()
+    
+    device = 'cuda:0'
+    batch_size = 16
+    _, test_loader, _ = prepare_data(batch_size)
+    
+    model = torch.load('./0.9099_deit3_small_patch16_224.pth', map_location='cpu', weights_only=False)
+    model = model.to(device)
+    model.eval()
+    
+    # Config to align HQQ 
+    model.device = 'cuda:0'
+    model.dtype = torch.float32
+    ##################################### 
+
+    # TODO: Quantize
+    quant_config = get_quant_config_deit(model)
+    
+    AutoHQQTimmModel.quantize_model(model, quant_config=quant_config, compute_dtype=torch.float32, device=device)
+    
+    from hqq.utils.patching import prepare_for_inference
+    prepare_for_inference(model)
+    torch.cuda.empty_cache()
+    
+    acc_after_quant = evaluate_model(model, test_loader, 'cuda:0')
+    print(f'Accuracy After Quant: {acc_after_quant}%')
+    print(f'Model Size (MiB) {get_size_of_model(model)/ (1024 ** 2)} MiB')
+    print(f'Your Score: {20 - max(90 - acc_after_quant, 0) * 10 + 17 - (get_size_of_model(model) / (1024 ** 2))}')
+
+    
+if __name__ == '__main__':
+    main()
